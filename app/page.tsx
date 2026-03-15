624d5653d14d5be4e20e79c609aec320fa778859
ifeelsam <sanskarsharma9005@gmail.com>
2026-03-16 03:51:58 +0530

    test: run

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
       <ComparisonSection />
       <BuiltOn />
       <DeveloperSection />
-      {/* <Testimonials /> */}
+      <Testimonials />
     </>
   )
 }
